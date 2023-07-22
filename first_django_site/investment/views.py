fadf02cea70e707fed662a51f0cee232eccc23ed
Assiriss <nikitos28t@gmail.com>
2023-07-22 14:07:27 +0300

    2 страницы главная и invesment

--- first_django_site/investment/views.py
+++ first_django_site/investment/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 
 def first(request):
     return HttpResponse('Страница прилодения Investment')
+
+def lot(request):
+    return HttpResponse("<h1>Привет бездари!</h1> "
+                        "<p>gf gf gf gf </p> "
+                        "<big>папапапапа</big>")
